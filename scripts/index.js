e18b03b0ccaaa24e7892ddb6ca67094224ec1590
George Adamov <george.adamov@gmail.com>
2025-01-14 17:34:50 +0300

    в процессе

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
+
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content; 
+//console.log(cardTemplate);
 
 // @todo: DOM узлы
+let places__container = document.querySelector('.places');
+//console.log(places__container);
+let place__item = places__container.querySelector('.places__list')
+//console.log(place__item);
 
 // @todo: Функция создания карточки
-
+function create__card(template, data) {
+    const user__places__item = template.querySelector('.places__item').cloneNode(true);
+    user__places__item.querySelector('.card__image').src = data.link;
+    user__places__item.querySelector('.card__image').alt = data.name;
+    user__places__item.querySelector('.card__title').textContent = data.name;
+    const button__delete = user__places__item.querySelector('.card__delete-button');
+    button__delete.addEventListener('click', function (event) { 
+        console.log(event);
+        //const deleteItem = button__delete.closest('user__places__item');
+        user__places__item.remove(); 
+        //delete__function(user__places__item);
+    
+    }); 
+    return user__places__item;
+}
 // @todo: Функция удаления карточки
+function delete__object(object) {
+    object.remove(); 
+}
 
 // @todo: Вывести карточки на страницу
+
+place__item.append(create__card(cardTemplate, initialCards[0]), callbak);
+place__item.append(create__card(cardTemplate, initialCards[1]), callbak);
+place__item.append(create__card(cardTemplate, initialCards[2]), callbak);
+place__item.append(create__card(cardTemplate, initialCards[3]), callbak);
