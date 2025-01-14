e489dcc8011e62929fe77567c898159e9f83e1ba
George Adamov <george.adamov@gmail.com>
2025-01-14 22:35:49 +0300

    рабочая версия

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,29 @@
 
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content; 
-//console.log(cardTemplate);
-
 // @todo: DOM узлы
 let places__container = document.querySelector('.places');
-//console.log(places__container);
 let place__item = places__container.querySelector('.places__list')
-//console.log(place__item);
 
 // @todo: Функция создания карточки
-function create__card(template, data, delete__method) {
+function create__card(template, data, f) {
     const user__places__item = template.querySelector('.places__item').cloneNode(true);
     user__places__item.querySelector('.card__image').src = data.link;
     user__places__item.querySelector('.card__image').alt = data.name;
     user__places__item.querySelector('.card__title').textContent = data.name;
     const button__delete = user__places__item.querySelector('.card__delete-button');
-    button__delete.addEventListener('click', function (event) { 
-        console.log(event);
-        //const deleteItem = button__delete.closest('user__places__item');
-        user__places__item.remove(); 
-        delete__method();
-        //delete__function(user__places__item);
-    
+    button__delete.addEventListener('click', function (event) {
+        f(user__places__item);
     }); 
     return user__places__item;
 }
 // @todo: Функция удаления карточки
-function delete__object() {
-    //object.remove();
-    console.log('delete object'); 
+var delete__object = function (object) {
+    object.remove();
 }
 
 // @todo: Вывести карточки на страницу
 
-place__item.append(create__card(cardTemplate, initialCards[0]), delete__object);
-place__item.append(create__card(cardTemplate, initialCards[1]), delete__object);
-place__item.append(create__card(cardTemplate, initialCards[2]), delete__object);
-place__item.append(create__card(cardTemplate, initialCards[3]), delete__object);
+initialCards.forEach((item) => {
+    place__item.append(create__card(cardTemplate, item, delete__object));
+  }); 
