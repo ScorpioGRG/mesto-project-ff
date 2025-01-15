fdb51478db5df3c8a2a971b66396eab01e3a9b5a
George Adamov <george.adamov@gmail.com>
2025-01-15 13:20:20 +0300

    Рабочая версия

--- scripts/index.js
+++ scripts/index.js
@@ -2,28 +2,28 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content; 
 // @todo: DOM узлы
-let places__container = document.querySelector('.places');
-let place__item = places__container.querySelector('.places__list')
+const placesContainer = document.querySelector('.places');
+const placeItem = placesContainer.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function create__card(template, data, f) {
-    const user__places__item = template.querySelector('.places__item').cloneNode(true);
-    user__places__item.querySelector('.card__image').src = data.link;
-    user__places__item.querySelector('.card__image').alt = data.name;
-    user__places__item.querySelector('.card__title').textContent = data.name;
-    const button__delete = user__places__item.querySelector('.card__delete-button');
+function createCardObject(template, data, deleteButtonHandler) {
+    const userPlacesItem = template.querySelector('.places__item').cloneNode(true);
+    userPlacesItem.querySelector('.card__image').src = data.link;
+    userPlacesItem.querySelector('.card__image').alt = data.name;
+    userPlacesItem.querySelector('.card__title').textContent = data.name;
+    const button__delete = userPlacesItem.querySelector('.card__delete-button');
     button__delete.addEventListener('click', function (event) {
-        f(user__places__item);
+        deleteButtonHandler(userPlacesItem);
     }); 
-    return user__places__item;
+    return userPlacesItem;
 }
 // @todo: Функция удаления карточки
-var delete__object = function (object) {
+const deleteObjectHandler = function (object) {
     object.remove();
 }
 
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach((item) => {
-    place__item.append(create__card(cardTemplate, item, delete__object));
+    placeItem.append(createCardObject(cardTemplate, item, deleteObjectHandler));
   }); 
