00fddfd5393e5ceb73052be77b73db1cade85319
George Adamov <george.adamov@gmail.com>
2025-01-20 19:46:01 +0300

    работа над замечаниями и небольшая оптимизация

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,17 @@
 
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content; 
+const getCardTemplate = () => { return document.querySelector('#card-template').content.querySelector('.places__item'); }
+const cardTemplateItem = getCardTemplate();
 // @todo: DOM узлы
 const placesContainer = document.querySelector('.places');
 const placeItem = placesContainer.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 function createCardObject(template, data, deleteButtonHandler) {
-    const userPlacesItem = template.querySelector('.places__item').cloneNode(true);
-    userPlacesItem.querySelector('.card__image').src = data.link;
-    userPlacesItem.querySelector('.card__image').alt = data.name;
+    const userPlacesItem = template.cloneNode(true);
+    const userPlacesImage = userPlacesItem.querySelector('.card__image');
+    userPlacesImage.src = data.link;
+    userPlacesImage.alt = data.name;
     userPlacesItem.querySelector('.card__title').textContent = data.name;
     const buttonDelete = userPlacesItem.querySelector('.card__delete-button');
     buttonDelete.addEventListener('click', function (event) {
@@ -25,5 +27,5 @@ const deleteObjectHandler = function (object) {
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach((item) => {
-    placeItem.append(createCardObject(cardTemplate, item, deleteObjectHandler));
+    placeItem.append(createCardObject(cardTemplateItem, item, deleteObjectHandler));
   }); 
